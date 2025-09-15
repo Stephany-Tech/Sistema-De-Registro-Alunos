5594f7eab7b74c80b1cc60abab5a90c69efa60ae
Stephany-Teste <stephany26bs@gmail.com>
2025-09-15 18:09:23 +0000

    criando codigos testes

--- criacao_de_banco_de_dados/banco_de_dados.sql
+++ criacao_de_banco_de_dados/banco_de_dados.sql
@@ -1,1 +1,14 @@
-insert into  
+CREATE TABLE Alunos (
+    ra INT NOT NULL PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    idade INT,
+    curso VARCHAR(50)
+);
+
+-- Inserindo alguns registros
+INSERT INTO Alunos (ra, nome, idade, curso) VALUES (1, 'João', 20, 'Sistemas');
+INSERT INTO Alunos (ra, nome, idade, curso) VALUES (2, 'Maria', 19, 'Design');
+INSERT INTO Alunos (ra, nome, idade, curso) VALUES (3, 'Pedro', 21, 'Administração');
+
+-- Consultando todos os alunos
+SELECT * FROM Alunos;
